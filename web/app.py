96b67bd671ac0233a424686d7efd5f73c1c02c26
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2019-04-27 23:53:12 +0500

    Added CORS Flask

--- web/app.py
+++ web/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
 from flask import request
 from services.jobs import fetch_latest_jobs
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
