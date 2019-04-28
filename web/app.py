d05f6df4dd8533f86350c1aa12c48e8b674e690d
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2019-04-28 17:42:50 +0500

    Added the count end point

--- web/app.py
+++ web/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask import request
-from services.jobs import fetch_latest_jobs
+from services.jobs import fetch_latest_jobs, get_total_jobs_count
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -11,6 +11,10 @@ def hello_world():
     page = int(request.args.get('page', 1))
     per_page = int(request.args.get('per_page', 10))
     return fetch_latest_jobs(page, per_page)
+
+@app.route('/jobs/count')
+def get_jobs_counts():
+    return get_total_jobs_count()
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
--- web/services/jobs.py
+++ web/services/jobs.py
@@ -3,7 +3,10 @@ from bson import json_util
 import json
 import pymongo
 from flask import jsonify
+from werkzeug.contrib.cache import RedisCache
+import sys
 
+cache = RedisCache(host='redis')
 client = MongoClient('mongo', 27017)
 db = client.remotejobs
 
@@ -13,3 +16,15 @@ def fetch_latest_jobs(page=1, per_page=10):
     jobs = json.dumps({'result': jobs, 'prev_url': '', 'next_url': ''}, indent=4, default=json_util.default)
 
     return jsonify(json.loads(jobs))
+
+def get_total_jobs_count():
+    rv = cache.get('total-jobs-count')
+    if rv is None:
+        print('Cache Miss', file=sys.stderr)
+        count = db.jobs.find().count()
+        response = json.dumps({'result': count})
+        cache.set('total-jobs-count', response, timeout=60 * 60 * 24)
+        return jsonify(json.loads(response))
+    
+    print('Cache Hit', file=sys.stderr)
+    return jsonify(json.loads(rv))
